29cc86859f65e3e7c85ad8116a000650019a7996
Edwin Tobias Ariza Tellez <edwin.ariza@pragma.com.co>
2023-05-31 13:06:39 -0500

    conexión a base de datos con variables de entorno, manejador de errores, lib class-validation y swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database/typeorm/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DEV_PG_PLAZA_HOST,
+      port: parseInt(process.env.DEV_PG_PLAZA_PORT),
+      username: process.env.DEV_PG_PLAZA_USERNAME,
+      password: process.env.DEV_PG_PLAZA_PASSWORD,
+      database: process.env.DEV_PG_PLAZA_DATABASE,
+      entities: [],
+      synchronize: false,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from '../util/http-exception.fillter';
+import * as dotenv from 'dotenv';
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
+  // definition of documentation library
+  const options = new DocumentBuilder()
+    .setTitle('API de plazoleta')
+    .setDescription('Documentación para api de plazoleta')
+    .setVersion('1.0')
+    // .addTag('usuarios')
+    // .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  const port = process.env.APP_PORT || 3000;
+  console.log(`listening on ${port}`);
   await app.listen(3000);
 }
 bootstrap();
